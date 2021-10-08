1ef5f3c064f507e09d7baa8b80cddc43a5e28239
fangDevAnd <13077995907@163.com>
2021-10-08 20:08:38 +0800

    提交内容

--- movie/web.js
+++ movie/web.js
@@ -70,64 +70,51 @@ addBack();
 
 let links = [
     {
-        src: "https://img1.baidu.com/it/u=3197755842,3611818723&fm=253&fmt=auto&app=120&f=PNG?w=300&h=300",
         name: "腾讯视频",
         link: "v.qq.com",
-        jsSrc:"http://https://fangdevand.github.io/movie/adblock/lezhu.js,",
-
     }, {
-        src: "https://img1.baidu.com/it/u=1789178533,84700239&fm=26&fmt=auto",
         name: "优酷视频",
         link: "www.youku.com/"
     }, {
-        src: "https://img2.baidu.com/it/u=3355477474,103047385&fm=26&fmt=auto",
         name: "爱奇艺",
         link: "www.iqiyi.com/"
     }, {
-        src: "https://img0.baidu.com/it/u=917710189,929080745&fm=26&fmt=auto",
         name: "六间房",
         link: "p.gouwuke.com/c?w=880701&c=7100&i=27442&pf=m&e=&t= www.6.cn"
     }, {
-        src: "https://img0.baidu.com/it/u=2778047413,2895491980&fm=26&fmt=auto",
         name: "土豆网",
         link: "www.5566.net/sp.htm#"
     },
     {
-        src: "https://img2.baidu.com/it/u=62140842,116155617&fm=26&fmt=auto",
         name: "乐视网",
         link: "www.5566.net/sp.htm#",
     },
     {
-        src: "https://img2.baidu.com/it/u=910208898,3772085318&fm=26&fmt=auto",
         name: "搜狐视频",
         link: "www.5566.net/sp.htm#",
     }, {
-        src: "https://img1.baidu.com/it/u=3308780222,3524051795&fm=26&fmt=auto",
         name: "百度视频",
         link: "v.baidu.com/",
     }, {
-        src: "https://img2.baidu.com/it/u=1626469276,1132249681&fm=26&fmt=auto",
+
         name: "56视频",
         link: "www.5566.net/sp.htm#"
     }, {
-        src: "https://img1.baidu.com/it/u=3574360052,3701577545&fm=15&fmt=auto",
         name: "哔哩哔哩",
         link: "www.bilibili.com",
     },
     {
-        src: "https://img2.baidu.com/it/u=3769294571,881364973&fm=26&fmt=auto",
         name: "芒果Tv",
-        link: "www.mgtv.com/",
+        link: "www.mgtv.com",
     }, {
-        src: "https://fangdevand.github.io/movie/az49q-gcinx.jpg",
-        name: "乐猪Tv",
-        link: "www.lezhutv.com/"
+        name: "lezhu",
+        link: "www.lezhutv.com"
     }
 ]
 
 for (let i = 0; i < links.length; i++) {
     if (links[i].link.indexOf(document.domain)) {
         //找到了对应的链接文件
-
     }
+    console.log("当前的domain", +document.domain);
 }
