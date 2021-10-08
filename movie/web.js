dc634e794dc34b5714ac991c2cdc50a6247d9618
fangDevAnd <13077995907@163.com>
2021-10-08 20:11:02 +0800

    提交内容

--- movie/web.js
+++ movie/web.js
@@ -116,5 +116,5 @@ for (let i = 0; i < links.length; i++) {
     if (links[i].link.indexOf(document.domain)) {
         //找到了对应的链接文件
     }
-    console.log("当前的domain", +document.domain);
+    alert("当前的domain" + document.domain);
 }
